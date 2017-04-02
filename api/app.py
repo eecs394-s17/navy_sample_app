c9c9d8c9edaec3dfee4260e0f2b41bd89a1a29e3
Robert Jin <robert.jin@vgbio.local>
2017-04-02 17:42:22 -0500

    post endpoint

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 import csv
+
 
 
 app = Flask(__name__)
@@ -11,25 +12,39 @@ class Notes(Resource):
 
     def __init__(self):
 
-        self.notes = []
+        self.notes_filepath = "notes.csv"
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('notes', type=str, required=True)
 
-        with open('notes.csv', 'rb') as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                # TODO: strip excess spaces
-                self.notes.append(row)
+        
 
     def get(self):
         """
         serves the get endpoint
         when hit it will return all the notes
         """
-
-        notes_json = { "notes" : self.notes }
+        notes = []
+        with open(self.notes_filepath, 'rb') as csvfile:
+                    reader = csv.reader(csvfile)
+                    for row in reader:
+                        # TODO: strip excess spaces
+                        notes.append(row)
+        notes_json = { "notes" : notes }
         return notes_json
 
     def post(self):
-        return "Not implemented"
+        """
+        """
+        args = self.parser.parse_args()
+        notes = args["notes"]
+
+        print notes
+
+        with open(self.notes_filepath, 'a') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([notes])
+
+        return "Notes Uploaded"
 
 
 api.add_resource(Notes, "/api/v1/notes")
