1057f9b0a335bbd3cd72bcd28cd4575663567557
sgp715 <7sebastianperez@gmail.com>
2017-04-03 12:48:59 -0500

    making simple before hosting

--- api/app.py
+++ api/app.py
@@ -8,11 +8,6 @@ import uuid
 
 import base64
 
-import boto3
-import sys, os
-from os import listdir
-from os.path import isfile, join
-from botocore.client import ClientError
 
 app = Flask(__name__)
 api = Api(app)
@@ -21,10 +16,6 @@ api = Api(app)
 class Notes(Resource):
 
     def __init__(self):
-
-        # Hit AWS
-        self.s3 = boto3.resource('s3')
-        self.client = boto3.client('s3')
 
         self.notes_filepath = "notes.csv"
         self.parser = reqparse.RequestParser()
@@ -37,13 +28,18 @@ class Notes(Resource):
         when hit it will return all the notes
         """
         notes = []
+
         with open(self.notes_filepath, 'rb') as csvfile:
                     reader = csv.reader(csvfile)
                     for row in reader:
                         # TODO: strip excess spaces
                         notes.append(row)
 
+        # TODO: should we get the image using base64?
+        # or should we go straight for hosting
+
         notes_json = { "notes" : notes }
+
         return notes_json
 
     def post(self):
@@ -60,22 +56,15 @@ class Notes(Resource):
         # save base64 to somewhere on machine
         # TODO: handle different filetypes
         unique_filename = str(uuid.uuid4())
-        # Upload to AWS bucket
-        bucket = self.s3.Bucket("team-navy")
-        exists = True
-        self.s3.meta.client.head_bucket(Bucket=bucket.name)
-        self.s3.Bucket(bucket.name).put_object(Key=unique_filename, Body=image)
+        with open(unique_filename, 'wb') as f:
+            f.write(image)
+
         # write file where image was saved to csv
         with open(self.notes_filepath, 'a') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow([unique_filename])
-        link_name = "https://s3.amazonaws.com/"+bucket.name+"/"+unique_filename
 
-        info = {"bucket": "team-navy",
-                "name": unique_filename}
-
-        return info
-
+        return { "path" : unique_filename }
 
 
 api.add_resource(Notes, "/api/v1/notes")
